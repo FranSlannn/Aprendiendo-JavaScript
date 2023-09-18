6c8bc96448885e7344bec7d0339379aa413ecaab
Fran <35973747+FranSlannn@users.noreply.github.com>
2023-09-18 12:29:46 +0000

    ejercicios

---
+++ Ejercicios/MiniProyectos.js
@@ -0,0 +1,39 @@
+// 1. Calculadora Simple
+// Descripción: Crea una calculadora en línea que realice operaciones básicas como suma, resta, multiplicación y división.
+// Implementación: Utiliza HTML para crear la interfaz de usuario y JavaScript para realizar los cálculos cuando los usuarios hagan clic en los botones.
+
+// 2. Lista de Tareas Interactiva
+// Descripción: Desarrolla una aplicación de lista de tareas que permita a los usuarios agregar, marcar como completadas y eliminar tareas.
+// Implementación: Utiliza HTML y CSS para la interfaz y JavaScript para la funcionalidad.
+
+// 3. Reloj Digital
+// Descripción: Crea un reloj digital que muestre la hora actual y se actualice automáticamente.
+// Implementación: Utiliza JavaScript para obtener la hora actual y actualizarla regularmente en la página.
+
+// 4. Galería de Imágenes
+// Descripción: Crea una galería de imágenes que permita a los usuarios hacer clic en una miniatura para ver la imagen en tamaño completo.
+// Implementación: Utiliza HTML, CSS y JavaScript para crear la galería y mostrar las imágenes.
+
+// 5. Conversor de Monedas
+// Descripción: Desarrolla una herramienta que convierta una moneda a otra, mostrando la tasa de cambio actual.
+// Implementación: Utiliza una API de tasas de cambio y JavaScript para realizar las conversiones.
+
+// 6. Generador de Contraseñas
+// Descripción: Crea una herramienta que genere contraseñas seguras con diferentes opciones, como longitud y tipos de caracteres.
+// Implementación: Utiliza JavaScript para generar contraseñas aleatorias y mostrarlas en la pantalla.
+
+// 7. Contador de Palabras
+// Descripción: Desarrolla una aplicación que cuente cuántas palabras hay en un texto ingresado por el usuario.
+// Implementación: Utiliza JavaScript para dividir el texto en palabras y contarlas.
+
+// 8. Mini Juego de Memoria
+// Descripción: Crea un juego de memoria en el que los usuarios tengan que encontrar las coincidencias entre cartas boca abajo.
+// Implementación: Utiliza HTML, CSS y JavaScript para crear el juego y gestionar las cartas.
+
+// 9. Temporizador Pomodoro
+// Descripción: Desarrolla un temporizador Pomodoro que ayude a los usuarios a gestionar su tiempo y tareas.
+// Implementación: Utiliza JavaScript para crear el temporizador y las funciones de inicio, pausa y reinicio.
+
+// 10. Generador de Cita del Día
+// Descripción: Crea una aplicación que muestre una cita inspiradora o divertida diferente cada día.
+// Implementación: Almacena una lista de citas y utiliza JavaScript para mostrar una cita aleatoria cada día.
---
+++ Ejercicios/ejercicios.js
@@ -0,0 +1,200 @@
+// Ejercicio 1: Variables y tipos de datos
+// Pregunta: ¿Cómo declaras una variable llamada `nombre` y asignas tu nombre a ella?
+// Respuesta:
+
+// Ejercicio 2: Condicionales
+// Pregunta: Escribe un programa que determine si un número es par o impar.
+// Respuesta:
+
+// Ejercicio 3: Bucles
+// Pregunta: Utiliza un bucle `for` para imprimir los números del 1 al 10.
+// Respuesta:
+
+// Ejercicio 4: Funciones
+// Pregunta: Crea una función que sume dos números y la llames con diferentes argumentos.
+// Respuesta:
+
+// Ejercicio 5: Arrays
+// Pregunta: Crea un array con nombres de frutas y muestra cada uno en la consola.
+// Respuesta:
+
+// Ejercicio 6: Condicionales
+// Pregunta: Escribe un programa que compare dos números y muestre el mayor.
+// Respuesta:
+
+// Ejercicio 7: Bucles
+// Pregunta: Escribe un programa que calcule la suma de los números del 1 al 100.
+// Respuesta:
+
+// Ejercicio 8: Funciones
+// Pregunta: Escribe una función que determine si un número es primo.
+// Respuesta:
+
+// Ejercicio 9: Arrays
+// Pregunta: Agrega un elemento a tu array de frutas y muestra la nueva lista.
+// Respuesta:
+
+// Ejercicio 10: Arrays
+// Pregunta: Elimina un elemento de tu array de frutas y muestra la nueva lista.
+// Respuesta:
+
+// Ejercicio 11: Variables y tipos de datos
+// Pregunta: ¿Cómo declaras una variable numérica llamada `edad` y le asignas un valor?
+// Respuesta:
+
+// Ejercicio 12: Condicionales
+// Pregunta: Escribe un programa que verifique si una variable `temperatura` es mayor o igual a 30 grados Celsius y muestre un mensaje apropiado.
+// Respuesta:
+
+// Ejercicio 13: Bucles
+// Pregunta: Utiliza un bucle `while` para imprimir los números del 1 al 5.
+// Respuesta:
+
+// Ejercicio 14: Funciones
+// Pregunta: Crea una función que multiplique dos números y la llames con diferentes argumentos.
+// Respuesta:
+
+// Ejercicio 15: Arrays
+// Pregunta: Crea un array con nombres de colores y muestra la cantidad de elementos en el array.
+// Respuesta:
+
+// Ejercicio 16: Condicionales
+// Pregunta: Escribe un programa que determine si un año es bisiesto o no.
+// Respuesta:
+
+// Ejercicio 17: Bucles
+// Pregunta: Utiliza un bucle `for` para imprimir los números pares del 1 al 20.
+// Respuesta:
+
+// Ejercicio 18: Funciones
+// Pregunta: Escribe una función que calcule el área de un triángulo dada su base y altura.
+// Respuesta:
+
+// Ejercicio 19: Arrays
+// Pregunta: Crea un array con números y encuentra el número más grande en el array.
+// Respuesta:
+
+// Ejercicio 20: Arrays
+// Pregunta: Crea un array con nombres de países. Utiliza un bucle `for` para imprimir cada país en la lista.
+// Respuesta:
+
+// Ejercicio 21: Condicionales
+// Pregunta: Escribe un programa que determine si un número es positivo, negativo o cero.
+// Respuesta:
+
+// Ejercicio 22: Bucles
+// Pregunta: Utiliza un bucle `for` para imprimir los primeros 10 números de la serie Fibonacci.
+// Respuesta:
+
+// Ejercicio 23: Funciones
+// Pregunta: Crea una función que calcule el factorial de un número.
+// Respuesta:
+
+// Ejercicio 24: Arrays
+// Pregunta: Crea dos arrays de números y luego crea un tercer array que contenga la suma de los elementos correspondientes de los dos primeros arrays.
+// Respuesta:
+
+// Ejercicio 25: Objetos
+// Pregunta: Crea un objeto `producto` con propiedades como nombre, precio y cantidad en stock. Calcula el valor total del producto (precio x cantidad).
+// Respuesta:
+
+// Ejercicio 26: Objetos
+// Pregunta: Crea un objeto `persona` con propiedades como nombre, edad y dirección. Luego, crea una función que imprima la información de la persona en un formato legible.
+// Respuesta:
+
+// Ejercicio 27: Eventos (para HTML y JavaScript)
+// Pregunta: Crea una página HTML con un botón. Utiliza JavaScript para hacer que al hacer clic en el botón, se cambie el color de fondo de la página.
+// Respuesta:
+
+// Ejercicio 28: Promesas
+// Pregunta: Crea una promesa que simule una operación asincrónica que tenga éxito o falle después de un cierto tiempo (usando `setTimeout`). Luego, maneja la promesa con `.then()` y `.catch()`.
+// Respuesta:
+
+// Ejercicio 29: Manejo de errores
+// Pregunta: Modifica una función para que genere un error personalizado y maneja ese error usando `try...catch`.
+// Respuesta:
+
+// Ejercicio 30: Algoritmos básicos
+// Pregunta: Escribe una función que invierta una cadena de texto. Por ejemplo, "hola" debería convertirse en "aloh".
+// Respuesta:
+
+// Ejercicio 31: Funciones y objetos
+// Desafío: Crea una función llamada `crearPersona` que tome un nombre y una edad como argumentos y devuelva un objeto persona con esas propiedades.
+// Respuesta:
+
+// Ejercicio 32: Arrays y bucles
+// Desafío: Crea una función llamada `encontrarMayor` que tome un array de números y devuelva el número más grande.
+// Respuesta:
+
+// Ejercicio 33: Eventos (para HTML y JavaScript)
+// Desafío: Crea una página HTML con un botón que diga "Clickeame". Usa JavaScript para mostrar una alerta cuando se haga clic en el botón.
+// Respuesta:
+
+// Ejercicio 34: Promesas y AJAX (opcional)
+// Desafío: Crea una promesa que simule una solicitud a una API externa (puedes usar `setTimeout`). Luego, usa `.then()` para mostrar los datos en la consola.
+// Respuesta:
+
+// Ejercicio 35: Algoritmos básicos
+// Desafío: Escribe una función que cuente cuántas veces aparece una palabra específica en una cadena de texto.
+// Respuesta:
+
+// Ejercicio 36: Algoritmos básicos
+// Desafío: Escribe una función que determine si una cadena de texto es un palíndromo (se lee igual de izquierda a derecha y de derecha a izquierda).
+// Respuesta:
+
+// Ejercicio 37: Objetos y métodos
+// Desafío: Crea un objeto `rectángulo` con propiedades `ancho` y `alto`. Luego, agrega un método `calcularArea` que devuelva el área del rectángulo.
+// Respuesta:
+
+// Ejercicio 38: Eventos (para HTML y JavaScript)
+// Desafío: Crea una página HTML con un input y un botón. Usa JavaScript para tomar el texto del input y mostrarlo en un párrafo cuando se haga clic en el botón.
+// Respuesta:
+
+// Ejercicio 39: Promesas y AJAX (opcional)
+// Desafío: Haz una solicitud AJAX a una API de tu elección y muestra los resultados en tu página HTML.
+// Respuesta:
+
+// Ejercicio 40: Algoritmos básicos
+// Desafío: Escribe una función que convierta una cadena de texto en formato "Camel Case" (por ejemplo, "esto_es_un_ejemplo" se convierte en "EstoEsUnEjemplo").
+// Respuesta:
+
+// Ejercicio 41: Objetos y métodos
+// Desafío: Crea un objeto `cuentaBancaria` con propiedades de saldo y una función `depositar` que permite agregar dinero a la cuenta. También, agrega una función `retirar` que permite retirar dinero, pero verifica si hay suficiente saldo.
+// Respuesta:
+
+// Ejercicio 42: Eventos y animaciones (para HTML y JavaScript)
+// Desafío: Crea una página HTML con un botón. Cuando se haga clic en el botón, haz que una imagen se desplace lentamente hacia la derecha utilizando animaciones de CSS.
+// Respuesta:
+
+// Ejercicio 43: Promesas y AJAX (opcional)
+// Desafío: Crea una aplicación que permita buscar imágenes usando la API de Unsplash. Muestra las imágenes en tu página y permite al usuario cargar más imágenes a medida que se desplaza.
+// Respuesta:
+
+// Ejercicio 44: Algoritmos avanzados
+// Desafío: Escribe una función que resuelva el problema de las Torres de Hanoi para un número dado de discos.
+// Respuesta:
+
+// Ejercicio 45: Manipulación avanzada del DOM (para HTML y JavaScript)
+// Desafío: Crea una galería de imágenes en una página HTML. Permite al usuario hacer clic en una imagen para verla en pantalla completa con un botón para cerrarla.
+// Respuesta:
+
+// Ejercicio 46: Programación funcional
+// Desafío: Implementa una función `map` personalizada que tome un array y una función de mapeo como argumentos y devuelva un nuevo array con los resultados de aplicar la función a cada elemento.
+// Respuesta:
+
+// Ejercicio 47: Programación orientada a objetos
+// Desafío: Crea una clase `Vehiculo` con propiedades como velocidad y capacidad de pasajeros. Luego, crea subclases para diferentes tipos de vehículos, como automóviles y bicicletas.
+// Respuesta:
+
+// Ejercicio 48: Juegos simples
+// Desafío: Crea un juego de adivinanza en el que el programa selecciona un número aleatorio y el jugador tiene que adivinarlo. Proporciona pistas si el número es mayor o menor.
+// Respuesta:
+
+// Ejercicio 49: Manipulación avanzada del DOM (para HTML y JavaScript)
+// Desafío: Crea una aplicación de lista de tareas que permita agregar, marcar como completadas y eliminar tareas. Utiliza el almacenamiento local para guardar las tareas.
+// Respuesta:
+
+// Ejercicio 50: Proyectos personales
+// Desafío: Elige un proyecto personal que te interese, como crear un blog simple, una aplicación de chat o cualquier idea que te emocione. Aplica lo que has aprendido en JavaScript para llevarlo a cabo.
+// Respuesta:
+
